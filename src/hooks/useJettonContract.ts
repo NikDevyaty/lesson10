2b0019f01ee10615e2f6d3d6f4d2382c43c387f1
nikolay.devyaty <nikolay.devyaty@fintopio.com>
2024-05-06 00:55:12 +0300

    init

--- src/hooks/useJettonContract.ts
+++ src/hooks/useJettonContract.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Address, fromNano, OpenedContract, toNano } from "ton-core";
-import {Mint, SampleJetton} from "../../build/SampleJetton/tact_SampleJetton";
-import {JettonDefaultWallet} from "../../build/SampleJetton/tact_JettonDefaultWallet";
+import { Mint, SampleJetton } from "../../build/SampleJetton/tact_SampleJetton";
+import { JettonDefaultWallet } from "../../build/SampleJetton/tact_JettonDefaultWallet";
 import { useAsyncInitialize } from "./useAsyncInitialize";
 import { useTonClient } from "./useTonClient";
 import { useTonConnect } from "./useTonConnect";
@@ -9,20 +9,20 @@ import { useTonConnect } from "./useTonConnect";
 const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time))
 
 export function useJettonContract() {
-    const {client} = useTonClient()
-    const {wallet, sender} = useTonConnect()
+    const { client } = useTonClient()
+    const { wallet, sender } = useTonConnect()
     const [balance, setBalance] = useState<string | null>()
 
-    const jettonContract = useAsyncInitialize(async()=>{
-        if(!client || !wallet) return;
+    const jettonContract = useAsyncInitialize(async () => {
+        if (!client || !wallet) return;
 
-        const contract = SampleJetton.fromAddress(Address.parse("EQB8StgTQXidy32a8xfu7j4HMoWYV0b0cFM8nXsP2cza_b7Y"))
-
+        // const contract = SampleJetton.fromAddress(Address.parse("EQB8StgTQXidy32a8xfu7j4HMoWYV0b0cFM8nXsP2cza_b7Y"))
+        const contract = SampleJetton.fromAddress(Address.parse("EQA3jXT_NMTu7_FJF0nLFL4VH6uCcPoXVcwpP3p0HHN8gHVG"))
         return client.open(contract) as OpenedContract<SampleJetton>
     }, [client, wallet])
 
-    const jettonWalletContract = useAsyncInitialize(async()=>{
-        if(!jettonContract || !client) return;
+    const jettonWalletContract = useAsyncInitialize(async () => {
+        if (!jettonContract || !client) return;
 
         const jettonWalletAddress = await jettonContract.getGetWalletAddress(
             Address.parse(Address.parse(wallet!).toString())
@@ -31,9 +31,9 @@ export function useJettonContract() {
         return client.open(JettonDefaultWallet.fromAddress(jettonWalletAddress))
     }, [jettonContract, client])
 
-    useEffect(()=>{
+    useEffect(() => {
         async function getBalance() {
-            if(!jettonWalletContract) return 
+            if (!jettonWalletContract) return
             setBalance(null)
             const balance = (await jettonWalletContract.getGetWalletData()).balance
             setBalance(fromNano(balance))
@@ -55,7 +55,7 @@ export function useJettonContract() {
             }
 
             jettonContract?.send(sender, {
-                value: toNano("0.05")
+                value: toNano("0.01")
             }, message)
         }
     }
